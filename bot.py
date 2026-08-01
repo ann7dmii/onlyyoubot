77752a97695db7eed876aa583aac389b4f627fab
ann7dmii <nesterovaana59@gmail.com>
2026-08-02 02:40:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
-print("Бот запущен!")
+import telebot
+TOKEN = "8849172686:AAEMt0SoscNpCVNF4KkNvu90Q7OkdjFeWG8"
+bot = telebot.Telebot(TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+  bot.send_message(message.chat.id, "привет, я запущен!!❤️")
+  bot.infinity_polling()
