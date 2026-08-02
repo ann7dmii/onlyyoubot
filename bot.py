826867fcde524d733896d34401c35f9c46643b00
ann7dmii <nesterovaana59@gmail.com>
2026-08-02 23:29:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,13 @@ def start(message):
     btn2 = types.InlineKeyboardButton("📸 наши моменты", callback_data="moments")
     btn3 = types.InlineKeyboardButton("💌 сообщение от Ани", callback_data="message")
     btn4 = types.InlineKeyboardButton("🌙 если тебе грустно", callback_data="sad")
+    btn5 = types.InlineKeyboardButton("💌 открыть, когда...", callback_data="open_when")
 
     keyboard.add(btn1)
     keyboard.add(btn2)
     keyboard.add(btn3)
     keyboard.add(btn4)
+    keyboard.add(btn5)
 
     bot.send_message(
         message.chat.id,
@@ -35,5 +37,35 @@ def callback(call):
 
     elif call.data == "message":
         bot.send_message(call.message.chat.id, "ты очень важный человек для меня ❤️")
+        elif call.data == "sad":
+    bot.send_message(
+        call.message.chat.id,
+        "🌙 если однажды тебе станет грустно...\n\n"
+        "🤍 просто открой этого бота.\n\n"
+        "💗 я надеюсь, что хотя бы одно сообщение отсюда сможет вызвать у тебя улыбку.\n\n"
+        "🫂 даже если меня нет рядом, мысленно я всегда тебя обнимаю.\n\n"
+        "😽 ты самый дорогой человек в моей жизни.\n\n"
+        "💘 я очень сильно тебя люблю.\n\n"
+        "✨ и никогда не забывай, какой ты замечательный."
+    )
+
+elif call.data == "open_when":
+    keyboard = types.InlineKeyboardMarkup()
+
+    btn1 = types.InlineKeyboardButton("🌧️ когда тебе грустно", callback_data="sad2")
+    btn2 = types.InlineKeyboardButton("🥺 когда скучаешь по мне", callback_data="miss")
+    btn3 = types.InlineKeyboardButton("😴 когда не можешь уснуть", callback_data="sleep")
+    btn4 = types.InlineKeyboardButton("💪 когда нужна поддержка", callback_data="support")
+
+    keyboard.add(btn1)
+    keyboard.add(btn2)
+    keyboard.add(btn3)
+    keyboard.add(btn4)
+
+    bot.send_message(
+        call.message.chat.id,
+        "💌 выбери момент, в который хочешь открыть письмо 🤍",
+        reply_markup=keyboard
+    )
 
 bot.infinity_polling()
