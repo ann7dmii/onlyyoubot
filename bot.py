e3685d968a12ab6f18de6b5e012ff209c55b0d27
ann7dmii <nesterovaana59@gmail.com>
2026-08-02 03:07:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,37 @@
 import telebot
+from telebot import types
+
 TOKEN = "8849172686:AAEMt0SoscNpCVNF4KkNvu90Q7OkdjFeWG8"
-bot = telebot.Telebot(TOKEN)
+
+bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(commands=['start'])
 def start(message):
-  bot.send_message(message.chat.id, "привет, я запущен!!❤️")
-  bot.infinity_polling()
+    keyboard = types.InlineKeyboardMarkup()
+
+    btn1 = types.InlineKeyboardButton("❤️ почему я тебя люблю", callback_data="love")
+    btn2 = types.InlineKeyboardButton("📸 наши моменты", callback_data="moments")
+    btn3 = types.InlineKeyboardButton("💌 сообщение от Ани", callback_data="message")
+
+    keyboard.add(btn1)
+    keyboard.add(btn2)
+    keyboard.add(btn3)
+
+    bot.send_message(
+        message.chat.id,
+        "привет, Тима ❤️\nя маленький подарок от Ани 😽",
+        reply_markup=keyboard
+    )
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data == "love":
+        bot.send_message(call.message.chat.id, "я люблю тебя за то, какой ты есть ❤️")
+
+    elif call.data == "moments":
+        bot.send_message(call.message.chat.id, "тут скоро будут наши самые тёплые моменты 📸")
+
+    elif call.data == "message":
+        bot.send_message(call.message.chat.id, "ты очень важный человек для меня ❤️")
+
+bot.infinity_polling()
